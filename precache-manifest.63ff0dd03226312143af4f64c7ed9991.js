4167e44cb0bcbfe5e2762287d18db7b6f2e24993
Danielle Sublett <danielle.sublett@gmail.com>
2020-03-27 14:27:12 -0700

    Updates

---
+++ precache-manifest.63ff0dd03226312143af4f64c7ed9991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde7e4ee5f0287a6ec597078e605c6f6",
+    "url": "/favorite-movies/index.html"
+  },
+  {
+    "revision": "30cd620788d19284b58d",
+    "url": "/favorite-movies/static/css/main.af2bd022.chunk.css"
+  },
+  {
+    "revision": "feedecb10686d8c91c38",
+    "url": "/favorite-movies/static/js/2.9bf87501.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/favorite-movies/static/js/2.9bf87501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30cd620788d19284b58d",
+    "url": "/favorite-movies/static/js/main.7efe07fd.chunk.js"
+  },
+  {
+    "revision": "4fbce03e7c4a473f5935",
+    "url": "/favorite-movies/static/js/runtime-main.2998d94e.js"
+  }
+]);
